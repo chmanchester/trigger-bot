038de2e4e1fee9c3ffa38e34b78a5187b3f2fd48
Chris Manchester <chmanchester@gmail.com>
2015-11-02 20:51:02 -0800

    Revert "Bug 1208104 - Re-introduce windows skipping"
    This reverts commit b1d2d1ed84912f1210fbc382e592ba877b2ce464.

--- triggerbot/triggerbot_pulse.py
+++ triggerbot/triggerbot_pulse.py
@@ -35,8 +35,6 @@ def extract_payload(payload, key):
             builder = prop[1]
         if prop[0] == 'branch':
             branch = prop[1]
-        if prop[0] == 'platform':
-            platform = prop[1]
 
     if rev and len(rev) > 12:
         rev = rev[:12]
@@ -59,11 +57,6 @@ def extract_payload(payload, key):
                              branch)
     match = unittest_re.match(key)
 
-    # Bug 1208104: Force triggering off for Windows testers until
-    # we have control of the backlog.
-    if platform.startswith('win64') or platform.startswith('win32'):
-        match = None
-
     return branch, rev, builder, status, match is not None, comments, user
 
 
@@ -77,6 +70,8 @@ def handle_message(data, message):
     if not all([branch == 'try',
                 is_test]):
         return
+
+    # logger.info('Saw %s at %s with "%s"' % (user, rev, comments))
 
     tw.handle_message(key, branch, rev, builder, status, comments, user)
 
